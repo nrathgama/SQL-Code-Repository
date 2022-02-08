f5202bbc8852372a5061e2995a0fe585b9b11480
NILU RA <70004867+nrathgama@users.noreply.github.com>
2022-02-08 01:04:33 -0600

    Add files via upload

---
+++ Joins-Inner_full_left_right_Outer.sql
@@ -0,0 +1,33 @@
+/*
+Inner Joins, Full/Left/Right Outer Joins
+*/
+
+SELECT *
+FROM [SQL Tutorial Project].dbo.EmployeeDemographics
+
+SELECT *
+FROM [SQL Tutorial Project].dbo.EmployeeSalary
+
+/*Everything overlaps with both tables*/
+SELECT * 
+FROM [SQL Tutorial Project].dbo.EmployeeDemographics
+Inner Join [SQL Tutorial Project].dbo.EmployeeSalary
+	ON EmployeeDemographics.EmployeeID = EmployeeSalary.EmployeeID
+
+SELECT * 
+FROM [SQL Tutorial Project].dbo.EmployeeDemographics
+Full Outer Join [SQL Tutorial Project].dbo.EmployeeSalary
+	ON EmployeeDemographics.EmployeeID = EmployeeSalary.EmployeeID
+
+/*Evrything on Employees Demographic table*/
+SELECT * 
+FROM [SQL Tutorial Project].dbo.EmployeeDemographics
+Left Outer Join [SQL Tutorial Project].dbo.EmployeeSalary
+	ON EmployeeDemographics.EmployeeID = EmployeeSalary.EmployeeID
+
+/*Evrything on Employees Salary table*/
+SELECT * 
+FROM [SQL Tutorial Project].dbo.EmployeeDemographics
+Left Outer Join [SQL Tutorial Project].dbo.EmployeeSalary
+	ON EmployeeDemographics.EmployeeID = EmployeeSalary.EmployeeID
+
